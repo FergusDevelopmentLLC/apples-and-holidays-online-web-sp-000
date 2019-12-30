bfa08bfa61fdbfe05febddafed1452206418ca1c
Will Carter <will.carter@fergusllc.com>
2019-12-30 01:50:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   end
 end
 
-add_supply_to_winter_holidays(holiday_supplies, "Balloons")
+add_supply_to_winter_holidays(holiday_hash, "Balloons")
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
