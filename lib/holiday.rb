8da45af839c472cb8b58a108f9ddc31fd3ef6487
Will Carter <will.carter@fergusllc.com>
2019-12-30 01:55:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,9 +44,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holidays|
     if season == :winter
-      holidays.each do |holiday|
-        puts holiday
-      end
+      puts season
+      # holidays.each do |holiday|
+      #   puts holiday
+      # end
     end
   end
 end
