8b28d5db68f65db3a52687c06069aae921fd7a77
Will Carter <will.carter@fergusllc.com>
2019-12-30 01:51:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holidays|
-    puts season
+    if season == "winter"
+      puts holidays
+    end
   end
 end
 
